56080445db6ebf5a4fbedcf9e559df1241be6b9d
Evgeniya Skripaleva <evgeniya_ignaten@mail.ru>
2022-02-04 16:28:01 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -35,12 +35,36 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': '*'
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [...expr];
+    let dot = '10';
+    let dashes = '11';
+    let space = '**';
+    let arrOfLetters = [];
+    while (arr.length) {
+        let arrOfPairs = [];
+        for (let i = 0; i < 5; i++) {
+            arrOfPairs.push(arr.splice(0, 2).join(''));
+        }
+        arrOfLetters.push(arrOfPairs)
+    }
+    let arrOfWords = arrOfLetters.map(letter => {
+        let res = letter.map(item => {
+            if (item === '11') return item.replace(/11/, '-');
+            if (item === '10') return item.replace(/10/, '.');
+            return item.replace(/00/ig, '');
+        });
+        return res.join('')
+    });
+    let res = arrOfWords.map(word => {
+        return MORSE_TABLE[word]
+    });
+    return res.join('').split('*').join(' ')
 }
 
 module.exports = {
     decode
-}
+};
